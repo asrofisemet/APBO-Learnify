0a8a0c5296c9b38bc0462ce27108737309b82141
asrofisemet <asrofisemet@gmail.com>
2026-06-30 17:24:55 +0800

    Menghapus folder duplikat Website-E-Learning-master

--- Website-E-Learning-master/application/views/kelas/index.php
+++
@@ -1,6 +0,0 @@
-<h2>Data Kelas</h2>
-<ul>
-<?php foreach($kelas as $k){ ?>
-    <li>Kelas <?= $k ?></li>
-<?php } ?>
-</ul>
--- Website-E-Learning-master/fix_pass.php
+++
@@ -1,6 +0,0 @@
-<?php
-$hash = password_hash("123456", PASSWORD_DEFAULT);
-$mysqli = new mysqli("localhost", "root", "", "learnify");
-$mysqli->query("UPDATE guru SET password = '$hash'");
-echo "Passwords updated successfully.";
-?>
